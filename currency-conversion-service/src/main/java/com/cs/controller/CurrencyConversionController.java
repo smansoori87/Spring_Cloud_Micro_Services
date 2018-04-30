5aa77105479e3687d9f80ee1d8e5d6939ae29a5d
suhailmansoori <suhailmansoori@10INLPC0SP3SR.reg1.1bank.dbs.com>
2018-04-30 19:04:23 +0530

    Rest call from Server.

--- currency-conversion-service/src/main/java/com/cs/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/cs/controller/CurrencyConversionController.java
@@ -1,20 +1,37 @@
 package com.cs.controller;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.cs.model.CurrencyConverterBean;
 
 @RestController
 public class CurrencyConversionController {
 
-	@GetMapping("/currency-conversion/api/v1/from/{from}/to/{to}/quantity/{quantity}")
-	public CurrencyConverterBean currencyConverter(@PathVariable String from, @PathVariable String to,
-			@PathVariable BigDecimal quantity) {
+	@Autowired
+	Environment env;
 
-		return null;// new CurrencyConverterBean(10001L, from, to, BigDecimal.valueOf(10),quantity, 10, 0);
+	@GetMapping("/currency-conversion/api/v1/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConverterBean currencyConverter(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+
+		Map<String, String> uriVar = new HashMap<String, String>();
+		uriVar.put("from", from);
+		uriVar.put("to", to);
+		ResponseEntity<CurrencyConverterBean> re = new RestTemplate().getForEntity("http://localhost:8000/exchange-rate/api/v1/from/{from}/to/{to}",
+				CurrencyConverterBean.class, uriVar);
+		CurrencyConverterBean ccb = re.getBody();
+		System.out.println(">>>>>>>>>>>" + ccb);
+
+		return new CurrencyConverterBean(ccb.getId(), from, to, ccb.getConversionMultiple(), quantity, ccb.getConversionMultiple().multiply(quantity),
+				Integer.valueOf(env.getProperty("local.server.port")));
 	}
 }
--- currency-exchange-rate-service/src/main/java/com/ce/controller/ExchangeController.java
+++ currency-exchange-rate-service/src/main/java/com/ce/controller/ExchangeController.java
@@ -25,14 +25,14 @@ public class ExchangeController {
 
 	@GetMapping("/exchange-rate/api/v1/from/{from}/to/{to}")
 	public Exchange currencyExchange(@PathVariable String from, @PathVariable String to) {
-		System.out.println(">>>>>>>>>"+cem);
-		ExchangeModel em = new ExchangeModel(1000L, from, to, cem.getConversionMultiple(),
-				Integer.valueOf(env.getProperty("local.server.port")));
-		exc = em;
+//		System.out.println(">>>>>>>>>"+cem);
+//		ExchangeModel em = new ExchangeModel(1000L, from, to, cem.getConversionMultiple(),
+//				Integer.valueOf(env.getProperty("local.server.port")));
+//		exc = em;
 
-		/*ExchangeRate er = cevr.findByFromAndTo(from, to);
+		ExchangeRate er = cevr.findByFromAndTo(from, to);
 		er.setPort(Integer.valueOf(env.getProperty("local.server.port")));
-		exc = er;*/
+		exc = er;
 
 		return exc;
 	}
