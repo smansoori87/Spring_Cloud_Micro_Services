675bd69d1ac815d84c099c2e89e61363a1b472e6
Suhail <Suhail@Suhail-PC>
2018-05-16 23:55:06 +0530

    Eureka NameServer.

--- currency-exchange-rate-service/src/main/java/com/ce/CurrencyExchangeServiceApplication.java
+++ currency-exchange-rate-service/src/main/java/com/ce/CurrencyExchangeServiceApplication.java
@@ -2,10 +2,12 @@ package com.ce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(value="com.ce")
+@ComponentScan(value = "com.ce")
+@EnableDiscoveryClient
 public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
