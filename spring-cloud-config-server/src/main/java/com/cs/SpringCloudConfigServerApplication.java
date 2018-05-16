29c6802d2c08b09a98c35ac79f46d05e52fb66c3
Suhail <Suhail@Suhail-PC>
2018-05-17 00:16:26 +0530

    .

--- spring-cloud-config-server/src/main/java/com/cs/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/cs/SpringCloudConfigServerApplication.java
@@ -2,11 +2,13 @@ package com.cs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableConfigServer
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan("com.cs")
 public class SpringCloudConfigServerApplication {
 
