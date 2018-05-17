dc0b480bac2a82b620d47fc325af08436ebc77b4
Suhail <Suhail@Suhail-PC>
2018-05-17 22:38:29 +0530

    Zuul API.

---
+++ netflix-zuul-api-gateway-server/src/main/java/com/ag/NetflixZuulApiGatewayServerApplication.java
@@ -0,0 +1,12 @@
+package com.ag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixZuulApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
+	}
+}
