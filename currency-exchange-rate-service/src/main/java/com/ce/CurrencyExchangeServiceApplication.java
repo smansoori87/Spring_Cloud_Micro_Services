3a3dc8a010ed3bea80a2e92ccefb0c2d223aed65
Suhail <Suhail@Suhail-PC>
2018-05-18 22:39:58 +0530

    Sleuth.

--- currency-exchange-rate-service/src/main/java/com/ce/CurrencyExchangeServiceApplication.java
+++ currency-exchange-rate-service/src/main/java/com/ce/CurrencyExchangeServiceApplication.java
@@ -3,7 +3,10 @@ package com.ce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @ComponentScan(value = "com.ce")
@@ -12,5 +15,11 @@ public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+
+	// To link centralized tracing id.
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 }
